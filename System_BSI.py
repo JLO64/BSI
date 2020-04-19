ef660213276d3d6a4fdcd160f62467d781e1234e
JLO64 <64julianlopez@gmail.com>
2020-04-18 18:11:20 -0700

    Basic System-BSI set up

--- System_BSI.py
+++ System_BSI.py
@@ -1,24 +1,36 @@
-import os, terminalColor
+import os, terminalColor, settingsJson
 try:
     import boto3
 except:
     os.system('pip3 install boto3')
     import boto3
 
-print("SYSTEM BSI\nMade By: Julian Lopez\nVersion: Origin")
-intDecision = 0
-listOfOptions = ["Set up a new computer", "Exit"]
+def SetupNewComputer():
+    terminalColor.printRedString("\nunable to connect to FATIMA")
+    terminalColor.printGreenString("Initiallizing System-BSI")
+    os.system('sudo apt update')
+    os.system('sudo apt upgrade')
+    toDownload = ["aptitude", "snap", "lynx", "vim"]
+    for i in toDownload:
+        os.system('sudo apt install ' + i)
 
-while ( ( (intDecision < 1) or (intDecision > len(listOfOptions)) ) ):
-        try:
-            print("\nWhat do you want to do?")
-            for i in range( len(listOfOptions) ):
-                terminalColor.printBlueString( str(i+1) + ". " + listOfOptions[i] )
-            intDecision = int(input())
-            if ( (intDecision < 1) or (intDecision > len(listOfOptions)) ): terminalColor.printRedString("Invalid Input")
-            elif ( listOfOptions[intDecision-1] == "Exit"): break #Exit program
-            else:
-                intDecision = 0    
-        except:
-            intDecision = 0
-            terminalColor.printRedString("Invalid Input")
+if __name__ == "__main__":
+    print("SYSTEM BSI(Bash Script Installer)\nMade By: Julian Lopez\nVersion: " + settingsJson.version)
+    intDecision = 0
+    listOfOptions = ["Set up a new computer","Browse Database","Exit"]
+
+    while ( ( (intDecision < 1) or (intDecision > len(listOfOptions)) ) ):
+            try:
+                print("\nWhat do you want to do?")
+                for i in range( len(listOfOptions) ):
+                    terminalColor.printBlueString( str(i+1) + ". " + listOfOptions[i] )
+                intDecision = int(input())
+                if ( (intDecision < 1) or (intDecision > len(listOfOptions)) ): terminalColor.printRedString("Invalid Input")
+                elif ( listOfOptions[intDecision-1] == "Exit"): break #Exit program
+                elif ( listOfOptions[intDecision-1] == "Set up a new computer"):
+                    SetupNewComputer()
+                else:
+                    intDecision = 0    
+            except:
+                intDecision = 0
+                terminalColor.printRedString("Invalid Input")
