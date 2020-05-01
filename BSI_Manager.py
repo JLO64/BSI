1ca0f074c214c7c2dffa3ccbabd07a6381399256
JLO64 <64julianlopez@gmail.com>
2020-04-30 21:32:59 -0700

    Added comments, consolidated into one file

--- BSI_Manager.py
+++ BSI_Manager.py
@@ -1,9 +1,50 @@
 #Made by Julian Lopez(JLO64)
 import os, terminalColor, settingsJson, boto3, sys
 from os import path
-import SystemBSI
 
 #https://starcatcher.us/TPT/Download/Snapshot%20linux64.zip
+
+
+#
+#   All of the functions/variables related to installing configuring computers
+#
+
+SystemBSItoDownloadAPT = ["aptitude", "snap", "lynx", "vim", "blueman"]
+
+def SystemBSI():
+    terminalColor.printCyanString("Initializing System-BSI")
+    
+    #Upgrading sofware on computer via apt
+    terminalColor.printCyanString("\nUpgrading software via apt") 
+    os.system('sudo apt update')
+    os.system('sudo apt upgrade -y')
+    os.system('sudo apt autoremove -y')
+
+    #Downloading sofware on computer via apt
+    for i in SystemBSItoDownloadAPT:
+        terminalColor.printCyanString("\nInstalling: " + i )
+        os.system('sudo apt install ' + i + " -y")
+
+    #Upgrading sofware on computer via snap
+    terminalColor.printCyanString("\nUpgrading software via snap")
+    os.system('sudo snap refresh')
+
+    #Changes wallpaper
+    terminalColor.printCyanString("\nChanging default wallpaper")
+    folderLocation = path.dirname(__file__)
+    os.system('sudo cp ' + folderLocation + '/System_Files/System-BSI_v1.png /usr/share/lubuntu/wallpapers/lubuntu-default-wallpaper.png')
+
+def downloadSelectedBSIs(listOfSelectedBSI):
+    #This is a placeholder for future internet features
+    terminalColor.printRedString("\nunable to connect to BSI-Servers")
+
+    #runs all BSIs in the list listOfSelectedBSI
+    for i in listOfSelectedBSI:
+        exec(str(i.replace(" ","") + "()" ))
+    
+    #Reminds user to apply all changes
+    terminalColor.printCyanString("\nPlease restart the computer to apply all changes made")
+    os.system('sleep 5s')
 
 def BSISelector():
     #Initializing variables
@@ -33,7 +74,7 @@ def BSISelector():
             elif( intDecision <= len(listOfAllBSI) ):#Has selected a BSI
                 
                 #Display info on selected BSI
-                print("\n" + listOfAllBSI[intDecision-1] + listOfBSIComments[intDecision-1] + "\nInstalls: " + ', '.join(eval( str(listOfAllBSI[intDecision-1]).replace(" ","") + ".toDownloadAPT" )) + listOfBSIChanges[intDecision-1] )
+                print("\n" + listOfAllBSI[intDecision-1] + listOfBSIComments[intDecision-1] + "\nInstalls: " + ', '.join(eval( str(listOfAllBSI[intDecision-1]).replace(" ","") + "toDownloadAPT" )) + listOfBSIChanges[intDecision-1] )
                 if not(listOfAllBSI[intDecision-1] == "System BSI"):
                     
                     #Ask if wants to download BSI
@@ -48,16 +89,8 @@ def BSISelector():
 
                 #Install Selected BSIs
                 if( commandSelection == 0 ):
-                    
-                    #runs all BSIs in the list listOfSelectedBSI
-                    for i in listOfSelectedBSI:
-                        hasSelectedBSIs = True
-                        exec(str(i.replace(" ","") + "." + i.replace(" ","") + "()" ))
-                    
-                    #Reminds user to apply all changes
-                    terminalColor.printCyanString("\nPlease restart the computer to apply all changes made")
-                    os.system('sleep 5s')
-                
+                    downloadSelectedBSIs(listOfSelectedBSI)
+                    hasSelectedBSIs = True
                 #Reset Selection
                 elif( commandSelection == 1 ):
                     listOfSelectedBSI=["System BSI"]
@@ -68,6 +101,9 @@ def BSISelector():
         except:
             terminalColor.printRedString("Invalid Input")
 
+#
+#   All of the functions related to application settings
+#
 def Settings():
     intDecision = 0
     settingsOptions = ["Update Software", "Cancel"]
@@ -84,7 +120,7 @@ def Settings():
 
                 if ( (intDecision < 1) or (intDecision > len(settingsOptions)) ): terminalColor.printRedString("Invalid Input")
                 elif ( settingsOptions[intDecision-1] == "Cancel"): break #Exit settings
-                elif ( settingsOptions[intDecision-1] == "Update Software"):
+                elif ( settingsOptions[intDecision-1] == "Update BSI-Manager"):
                     intDecision = 0   
                     os.system( path.dirname(__file__) + '/BSI-Installer.sh')
                     sys.exit()
@@ -95,6 +131,9 @@ def Settings():
                 intDecision = 0
                 terminalColor.printRedString("Invalid Input")
 
+#
+#   The function that runs first when the program is run
+#
 if __name__ == "__main__":
     print("\nBSI(Bash Script Installer) Manager\nMade By: Julian Lopez\nVersion: " + settingsJson.version)
     intDecision = 0
